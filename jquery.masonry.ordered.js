6d830287a6206f8c21eeb8cb97d5670a56ff21e4
Vit Brunner <vit.brunner@gmail.com>
2012-08-12 17:52:24 +0200

    use jekyll!
    - add front page with simple demo
    - redo demo photo gallery page to fit in
    - move shit around (yes, should've been different commits...)

--- jquery.masonry.ordered.js
+++ jquery.masonry.ordered.js
@@ -1,5 +1,14 @@
-// JQuery Masonry Sorted plugin
-// Depends on: http://masonry.desandro.com/
+/**
+ * jQuery Masonry Ordered
+ * http://masonry-ordered.tasuki.org/
+ *
+ * Enhanced layout strategy for jQuery Masonry:
+ * http://masonry.desandro.com/
+ *
+ * Licensed under the MIT license.
+ * Copyleft 2012 Vit 'tasuki' Brunner
+ */
+
 (function( window, $, undefined ) {
 
   $.extend(true, $.Mason.settings, {
